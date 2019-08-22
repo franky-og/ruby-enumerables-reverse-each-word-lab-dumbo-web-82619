8a2ff16f2464af9f692d9b95bdee34d3b6531a1a
Franky Liu <cuifranky95@gmail.com>
2019-08-22 15:28:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   newstring = ""
   newarray = string.split(' ')
   newarray.each do |index|
-    newstring << index.reverse
+    newstring << index.reverse + " "
   end
   return newstring
 end
